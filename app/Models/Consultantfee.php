8f407dfaccec81b1e47232decd00abf070f142e0
Eco Galileo <erick@datahyv.com>
2022-10-20 19:16:40 +0800

    added consultantfees table

--- app/Models/Consultantfee.php
+++ app/Models/Consultantfee.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Consultantfee extends Model
 {
     use HasFactory;
+    protected $table = 'consultantfees';
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'lead_faci',
+        'co_lead',
+        'co_lead_f2f',
+        'co_faci',
+        'lead_consultant',
+        'consulting',
+        'designer',
+        'moderator',
+        'producer',
+    ];
 }
