5b088c14c61ee2d7c6e30db92ae5e6ccaaef0883
leejcode123 <johnleemontecillo@gmail.com>
2022-10-27 08:35:06 +0800

    stage commit

---
+++ database/migrations/2022_09_02_000414_create_cruz_pia_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+//CONSULTANT REVENUE TRACKER
+
+class CreateCruzPiaTable extends Migration 
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::create('cruz_pia', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+        Schema::create('cruz_pia', function (Blueprint $table) {
+            $table->id();
+        
+            $table->string('payment_date', 50)->nullable($value = true);
+            $table->string('engagement')->nullable($value = true);
+            $table->string('contract_fee')->nullable($value = true);
+            $table->string('ewt')->nullable($value = true);
+            $table->string('vat_exempt_sales')->nullable($value = true);
+            $table->string('total_amount_deposited')->nullable($value = true);
+            $table->string('role', 50)->nullable($value = true);
+            $table->boolean('paid')->nullable($value = true);
+            $table->string('or')->nullable($value = true);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cruz_pia');
+    }
+}
+
+
---
+++ database/migrations/2022_09_05_000423_create_francisco_ton_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+//CONSULTANT REVENUE TRACKER
+
+class CreateFranciscoTonTable extends Migration 
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::create('francisco_ton', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+        Schema::create('francisco_ton', function (Blueprint $table) {
+            $table->id();
+        
+            $table->string('payment_date', 50)->nullable($value = true);
+            $table->string('engagement')->nullable($value = true);
+            $table->string('contract_fee')->nullable($value = true);
+            $table->string('vat')->nullable($value = true);
+            $table->string('ewt')->nullable($value = true);
+            $table->string('vat_exempt_sales')->nullable($value = true);
+            $table->string('total_amount_deposited')->nullable($value = true);
+            $table->string('remarks', 50)->nullable($value = true);
+            $table->boolean('paid')->nullable($value = true);
+           
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('francisco_ton');
+    }
+}
+
+
+
